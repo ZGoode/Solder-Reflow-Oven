b48d99fcff99dbbf095708d2b8500fdd0e5388e3
Zachary Nathan Goode <zachary@the-goodes.com>
2019-03-29 13:23:41 -0700

    Update Solder-Reflow-Oven.ino

--- src/Solder-Reflow-Oven/Solder-Reflow-Oven.ino
+++ src/Solder-Reflow-Oven/Solder-Reflow-Oven.ino
@@ -17,7 +17,7 @@
 #include <LiquidCrystal.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int up = 6, down = 7, left = 8, right = 9, ok = 10, relay = 13, alwaysOn = 1;
+const int up = 6, down = 7, ok = 8, left = 9, right = 10, relay = 13, alwaysOn = 1;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #define THERMISTORPIN A0
@@ -31,11 +31,12 @@ uint16_t samples[NUMSAMPLES];
 
 int currentScreen = 0;
 int cursorPosition = 0;
-int hundreds = 0;
-int tens = 2;
+int hundreds = 2;
+int tens = 0;
 int ones = 0;
 int setTemp = 200;
 boolean firstDisplay = true;
+boolean firstAlwaysOn = true;
 
 long sampleTemp();
 
@@ -47,6 +48,7 @@ void setup() {
   pinMode(left, INPUT);
   pinMode(right, INPUT);
   pinMode(ok, INPUT);
+  pinMode(alwaysOn, INPUT);
 
   pinMode(relay, OUTPUT);
 
@@ -67,142 +69,177 @@ void setup() {
 }
 
 void loop() {
-  if (currentScreen == 0) { //home screen
-    if (firstDisplay == true) {
-      lcd.clear();
-      lcd.print("press select");
-      lcd.setCursor(0, 1);
-      lcd.print("to begin heating");
-      firstDisplay = false;
-    }
-
-    if (digitalRead(ok) == HIGH) {
-      currentScreen++;
-      lcd.clear();
-      firstDisplay = true;
-      delay(500);
-    }
-  } else if (currentScreen == 1) { //temperature select scree
-    if (firstDisplay == true) {
-      lcd.clear();
-      lcd.blink();
-      lcd.print("Set Temperature");
-      lcd.setCursor(0, 1);
-      int tempint = (hundreds * 100) + (tens * 10) + (ones);
-      String temp = String(tempint);
-      lcd.print(temp + " Celsius");
-      lcd.setCursor(0, 1);
-      firstDisplay = false;
-    }
-
-    if (digitalRead(up) == HIGH) {
-      if (cursorPosition == 0) {
-        if (hundreds < 4) {
-          hundreds++;
-        }
-      } else if (cursorPosition == 1) {
-        if (tens < 9) {
-          tens++;
-        }
-      } else if (cursorPosition == 2) {
-        if (ones < 9) {
-          ones++;
-        }
-      }
-      delay(250);
-    }
-
-    if (digitalRead(down) == HIGH) {
-      if (cursorPosition == 0) {
-        if (hundreds > 0) {
-          hundreds--;
-        }
-      } else if (cursorPosition == 1) {
-        if (tens > 9) {
-          tens--;
-        }
-      } else if (cursorPosition == 2) {
-        if (ones > 9) {
-          ones--;
-        }
-      }
-      delay(250);
-    }
-
-    if (digitalRead(left) == HIGH) {
-      if (cursorPosition > 0) {
-        cursorPosition--;
-        lcd.setCursor(cursorPosition, 1);
-      }
-      delay(250);
-    }
-
-    if (digitalRead(right) == HIGH) {
-      if (cursorPosition < 2) {
-        cursorPosition++;
-        lcd.setCursor(cursorPosition, 1);
-      }
-      delay(250);
-    }
-
-    if (digitalRead(ok) == HIGH) {
-      setTemp = (hundreds * 100) + (tens * 10) + (ones);
-      Serial.println("setTemp: ");
-      Serial.println(setTemp);
-      Serial.println(hundreds);
-      Serial.println(tens);
-      Serial.println(ones);
-      currentScreen++;
-      lcd.noBlink();
-      lcd.clear();
-      firstDisplay = true;
-      delay(500);
-    }
-  } else if (currentScreen == 2) { //heating in progress
-    long tempTemp = sampleTemp();
-
-    if (firstDisplay == true) {
-      digitalWrite(relay, HIGH);
-      lcd.clear();
-      lcd.print("Target Temp: " + String(setTemp));
-      lcd.setCursor(0, 1);
-      firstDisplay = false;
-      lcd.print("Temp now: " + String(tempTemp));
-    }
-
-    if (setTemp <= tempTemp) {
+  if (digitalRead(alwaysOn) == HIGH) {
+    if (firstAlwaysOn == false) {
       digitalWrite(relay, LOW);
-      currentScreen++;
-      firstDisplay = true;
-      lcd.clear();
-      delay(100);
-    }
-
-    delay(100);
-
-    lcd.setCursor(10, 1);
-    lcd.print(String(tempTemp) + "       ");
-  } else if (currentScreen == 3) { //done
-    long tempTemp = sampleTemp();
-    if (firstDisplay == true) {
-      lcd.clear();
-      lcd.print("Cooling Down");
-      lcd.setCursor(0, 1);
-      firstDisplay = false;
-      lcd.print("Temp: " + String(tempTemp));
-    }
-
-    if (50 > tempTemp) {
       currentScreen = 0;
+      lcd.setCursor(0, 0);
       lcd.clear();
-      firstDisplay = true;
-      delay(500);
+      firstAlwaysOn = true;
     }
+    if (currentScreen == 0) { //home screen
+      if (firstDisplay == true) {
+        lcd.clear();
+        lcd.print("press select");
+        lcd.setCursor(0, 1);
+        lcd.print("to begin heating");
+        firstDisplay = false;
+      }
 
-    delay(100);
+      if (digitalRead(ok) == HIGH) {
+        currentScreen++;
+        lcd.clear();
+        firstDisplay = true;
+        delay(500);
+      }
+    } else if (currentScreen == 1) { //temperature select scree
+      if (firstDisplay == true) {
+        lcd.clear();
+        lcd.blink();
+        lcd.print("Set Temperature");
+        lcd.setCursor(0, 1);
+        String temp = ((String)hundreds);
+        temp += ((String)tens);
+        temp += ((String)ones);
+        lcd.print(temp + " Celsius");
+        lcd.setCursor(0, 1);
+        firstDisplay = false;
+      }
 
-    lcd.setCursor(6, 1);
-    lcd.print(String(tempTemp) + "              ");
+      if (digitalRead(up) == HIGH) {
+        if (cursorPosition == 0) {
+          if (hundreds < 4) {
+            hundreds++;
+          }
+        } else if (cursorPosition == 1) {
+          if (tens < 9) {
+            tens++;
+          }
+        } else if (cursorPosition == 2) {
+          if (ones < 9) {
+            ones++;
+          }
+        }
+
+        lcd.setCursor(0, 1);
+        String temp = ((String)hundreds);
+        temp += ((String)tens);
+        temp += ((String)ones);
+        lcd.print(temp + " Celsius");
+        lcd.setCursor(cursorPosition, 1);
+
+        delay(250);
+      }
+
+      if (digitalRead(down) == HIGH) {
+        if (cursorPosition == 0) {
+          if (hundreds > 0) {
+            hundreds--;
+          }
+        } else if (cursorPosition == 1) {
+          if (tens > 9) {
+            tens--;
+          }
+        } else if (cursorPosition == 2) {
+          if (ones > 9) {
+            ones--;
+          }
+        }
+
+        lcd.setCursor(0, 1);
+        String temp = ((String)hundreds);
+        temp += ((String)tens);
+        temp += ((String)ones);
+        lcd.print(temp + " Celsius");
+        lcd.setCursor(cursorPosition, 1);
+
+        delay(250);
+      }
+
+      if (digitalRead(left) == HIGH) {
+        if (cursorPosition > 0) {
+          cursorPosition--;
+          lcd.setCursor(cursorPosition, 1);
+        }
+        delay(250);
+      }
+
+      if (digitalRead(right) == HIGH) {
+        if (cursorPosition < 2) {
+          cursorPosition++;
+          lcd.setCursor(cursorPosition, 1);
+        }
+        delay(250);
+      }
+
+      if (digitalRead(ok) == HIGH) {
+        setTemp = (hundreds * 100) + (tens * 10) + (ones);
+        Serial.println("setTemp: ");
+        Serial.println(setTemp);
+        Serial.println(hundreds);
+        Serial.println(tens);
+        Serial.println(ones);
+        currentScreen++;
+        lcd.noBlink();
+        lcd.clear();
+        firstDisplay = true;
+        delay(500);
+      }
+    } else if (currentScreen == 2) { //heating in progress
+      long tempTemp = sampleTemp();
+
+      if (firstDisplay == true) {
+        digitalWrite(relay, HIGH);
+        lcd.clear();
+        lcd.print("Target Temp: " + String(setTemp));
+        lcd.setCursor(0, 1);
+        firstDisplay = false;
+        lcd.print("Temp now: " + String(tempTemp));
+      }
+
+      if (setTemp <= tempTemp) {
+        digitalWrite(relay, LOW);
+        currentScreen++;
+        firstDisplay = true;
+        lcd.clear();
+        delay(100);
+      }
+
+      delay(100);
+
+      lcd.setCursor(10, 1);
+      lcd.print(String(tempTemp) + "       ");
+    } else if (currentScreen == 3) { //done
+      long tempTemp = sampleTemp();
+      if (firstDisplay == true) {
+        lcd.clear();
+        lcd.print("Cooling Down");
+        lcd.setCursor(0, 1);
+        firstDisplay = false;
+        lcd.print("Temp: " + String(tempTemp));
+      }
+
+      if (50 > tempTemp) {
+        currentScreen = 0;
+        lcd.clear();
+        firstDisplay = true;
+        delay(500);
+      }
+
+      delay(100);
+
+      lcd.setCursor(6, 1);
+      lcd.print(String(tempTemp) + "              ");
+    }
+  } else {
+    if (firstAlwaysOn == true) {
+      lcd.clear();
+      digitalWrite(relay, HIGH);
+      currentScreen = 0;
+      lcd.setCursor(0, 0);
+      lcd.print("Always On Mode");
+      firstAlwaysOn = false;
+    }
   }
 }
 
